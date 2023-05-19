d97efe2f7ad373fe8a03007f921b83877c77aeed
sarazaToussaint <tmsaraza@gmail.com>
2023-05-19 23:23:16 +0200

    Make change in 6-invokedModule.js file

--- 6-invokedModule.js
+++ 6-invokedModule.js
@@ -2,7 +2,7 @@ const num1 = 12;
 const num2 = 18;
 
 function sum (){
- console.log(`The total amount is: ${num1 + num2}`);
+ console.log(`The sum of num1 and num2 is: ${num1 + num2}`);
 }
 
 sum();
