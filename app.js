a38f97991b17a0d57c40ccaa769c9476de7aa2dc
sarazaToussaint <tmsaraza@gmail.com>
2023-05-19 23:17:10 +0200

    Create app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+const { country, city } = require('./4-names');
+const sayHello = require('./5-utils');
+const data = require('./alternative');
+require('./6-invokedModule')
+
+// console.log(data);
+// sayHello('Toussaint');
+// sayHello(country);
+// sayHello(city);
