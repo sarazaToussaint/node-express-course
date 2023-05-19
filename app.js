99d446b1c7d096e8aee831df477a0d51212bcdd4
sarazaToussaint <tmsaraza@gmail.com>
2023-05-19 23:20:35 +0200

    Uncomment codes in the app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const sayHello = require('./5-utils');
 const data = require('./alternative');
 require('./6-invokedModule')
 
-// console.log(data);
-// sayHello('Toussaint');
-// sayHello(country);
-// sayHello(city);
+console.log(data);
+sayHello('Toussaint');
+sayHello(country);
+sayHello(city);
