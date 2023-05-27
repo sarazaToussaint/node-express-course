f1353ff5429ad92ef07004f65b668dd2b96ada49
sarazaToussaint <tmsaraza@gmail.com>
2023-05-27 20:51:19 +0200

    Add global varibles in node

---
+++ Node-tutorial/1-intro.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("This is a small number");  
+}else {
+   console.log("This is a large number") 
+}
+
+console.log("This is my first Node.js app");
