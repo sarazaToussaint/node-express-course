9ee993de3faddfb3392b9a73006999359a7b8489
sarazaToussaint <tmsaraza@gmail.com>
2023-05-27 20:45:03 +0200

    Delete unused file

--- app.js
+++
@@ -1,9 +0,0 @@
-const { country, city } = require('./4-names');
-const sayHello = require('./5-utils');
-const data = require('./alternative');
-require('./6-invokedModule')
-
-// console.log(data);
-// sayHello('Toussaint');
-// sayHello(country);
-// sayHello(city);
