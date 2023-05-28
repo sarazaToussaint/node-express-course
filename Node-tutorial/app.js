c4caf2f2af6820526f0f589a857aa6e84e944fff
sarazaToussaint <tmsaraza@gmail.com>
2023-05-28 19:42:07 +0200

    Run error in console.log in app file

--- Node-tutorial/app.js
+++ Node-tutorial/app.js
@@ -8,6 +8,7 @@ readFile('./content/first.txt','utf8', (error, result) => {
   const first = result;
   readFile('./content/secont.txt', 'utf8', (error, result) => {
    console.log(error);
+   return
   })
 
 });
