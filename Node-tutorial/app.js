66773ef26d7c07bc40a0aae6701891d7d4d3c509
sarazaToussaint <tmsaraza@gmail.com>
2023-05-28 19:33:22 +0200

    use of fs module in node

--- Node-tutorial/app.js
+++ Node-tutorial/app.js
@@ -1,3 +1,9 @@
 const {readFile, writeFile} = require('fs');
 
-
+readFile('./content/first.txt','utf8', (error, result) => {
+  if(error){
+    console.log(error);
+    return
+  }
+  console.log(result);
+});
