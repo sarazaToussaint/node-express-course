e410f591bfb965b4d38e550aadc4ed8d806addba
sarazaToussaint <tmsaraza@gmail.com>
2023-05-29 06:48:56 +0200

    Remove unused codes in the app.js file

--- Node-tutorial/app.js
+++ Node-tutorial/app.js
@@ -1,10 +1,1 @@
 const {readFile, writeFile} = require('fs');
-
-readFile('./content/first.txt','utf8', (error, result) => {
-  if(error){
-    console.log(error);
-    return
-  }
-  console.log(result);
-});
-
