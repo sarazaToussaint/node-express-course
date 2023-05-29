9e9117fb873e27065a558670a3ad94f6f90a791d
sarazaToussaint <tmsaraza@gmail.com>
2023-05-29 08:35:34 +0200

    Use of http module in nodeJs

--- Node-tutorial/app.js
+++ Node-tutorial/app.js
@@ -1,1 +1,18 @@
-const {readFile, writeFile} = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/'){
+    res.end('Home page');
+  }else if(req.url === '/about'){
+    res.end('This is our about page')
+  }else{
+    res.end(`
+    <h1>Oops!<h1>
+    <p>The page you are looking for seems to be an accessible</p>
+
+    <a href='/'>Back Home</a>
+    `)
+  }
+})
+
+server.listen(5000);
