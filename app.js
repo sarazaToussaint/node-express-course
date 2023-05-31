e50d2ca4c1fd6300c580c11dc054cd09321b28bf
sarazaToussaint <tmsaraza@gmail.com>
2023-05-31 12:56:33 +0200

    Use of request event in node

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const EventEmitter = require('events');
+const http = require('http')
 
-const customEmitter = new EventEmitter();
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
 
-customEmitter.on('response', (name, id) => {
-  console.log(`Data received by ${name} with the id: ${id}`);
+// Using Event Emitter API
+const server = http.createServer()
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+  res.end('Welcome')
 })
 
-customEmitter.on('response', () => {
-  console.log('This is an other logic');
-})
-
-customEmitter.emit('response', 'John', 243)
+server.listen(5000);
