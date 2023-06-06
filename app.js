579f2c3f108c82fce4b0f6a7adff85a042efe55e
sarazaToussaint <tmsaraza@gmail.com>
2023-06-06 22:32:15 +0200

    Use of http-stream in nodeJS

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const { createReadStream } = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-const stream = createReadStream('./Node-tutorial/content/big.txt');
+http.createServer((req, res) => {
+  // const text = fs.readFileSync('./Node-tutorial/content/big.txt', 'utf8');
+  // res.end(text);
+  const fileStream = fs.createReadStream('./Node-tutorial/content/big.txt', 'utf8');
 
-stream.on('data', (result) => {
-  console.log(result);
-})
+  fileStream.on('open', () => {
+   fileStream.pipe(res);
+  })
+  fileStream.on('err', (err) => {
+    res.end(err)
+  })
+
+}).listen(5000);
+
