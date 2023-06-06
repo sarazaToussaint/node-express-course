67093762577600f8fffd35e437ca3a71019ca2a1
sarazaToussaint <tmsaraza@gmail.com>
2023-06-06 22:40:38 +0200

    Remove the unused code in App.js file

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-const http = require('http');
-const fs = require('fs');
-
-http.createServer((req, res) => {
-  // const text = fs.readFileSync('./Node-tutorial/content/big.txt', 'utf8');
-  // res.end(text);
-  const fileStream = fs.createReadStream('./Node-tutorial/content/big.txt', 'utf8');
-
-  fileStream.on('open', () => {
-   fileStream.pipe(res);
-  })
-  fileStream.on('err', (err) => {
-    res.end(err)
-  })
-
-}).listen(5000);
 
