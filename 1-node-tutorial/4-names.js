b35d5ce5199fb3a970fcdfcc4070c5fd6323a7a0
sarazaToussaint <tmsaraza@gmail.com>
2023-06-07 18:14:32 +0200

    Create 4-names.js file

---
+++ 1-node-tutorial/4-names.js
@@ -0,0 +1,17 @@
+// Local
+const secret = "SUSPECT SECRET";
+
+// Share
+const country = "Rwanda" ;
+const city = "Kigali";
+
+module.exports = { country, city };
+
+
+// // local
+// const secret = 'SUPER SECRET'
+// // share
+// const john = 'john'
+// const peter = 'peter'
+
+// module.exports = { john, peter }
