83132a626a994fde57118f3570d33344ff26f1da
sarazaToussaint <tmsaraza@gmail.com>
2023-06-08 22:52:45 +0200

    Create a server using express framework

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server listening to http:localhost:' + port);
+  console.log('Server listening to http:localhost:');
 });
