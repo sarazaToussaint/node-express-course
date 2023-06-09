e05ba9c8aa5cc043a8f9ecdfce3d9bc375bd3f4c
sarazaToussaint <tmsaraza@gmail.com>
2023-06-09 22:03:07 +0200

    Add Content-Type in writeHead function

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const routes = {
     '/': function home(req, res){
-         res.writeHead(200);
+         res.writeHead(200, {'Content-Type': 'text/plain'});
          res.end('Welcome to the Home Page');
     },
 
