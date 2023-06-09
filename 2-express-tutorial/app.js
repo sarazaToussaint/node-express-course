3f50e4548140a2e7978121dcb1f718ea811263bc
sarazaToussaint <tmsaraza@gmail.com>
2023-06-09 21:34:00 +0200

    Create an object of routes in app.js

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,12 +1,23 @@
-const express = require('express');
+const http = require('http');
 
-const app = express();
-const port = 5000;
+const routes = {
+    '/': function index(req, res){
+       res.writeHead(200);
+       res.end('Hello, World!');
+    },
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+    '/foo': function foo(req, res){
+        res.writeHead(200);
+        res.end('You are now viewing foo');
+    }
+}
 
-app.listen(port, () => {
-  console.log('Server listening to http:localhost:' + port);
-});
+http.createServer((req, res) => {
+  if(req.url in routes){
+    return routes[req.url](req, res);
+  }
+  
+  res.writeHead(404);
+  res.end(http.STATUS_CODES[404]);
+
+}).listen(process.env.PORT || 5000);
