db7a551e8ba891c9a873c3842c203620a81774a1
sarazaToussaint <tmsaraza@gmail.com>
2023-06-11 12:56:52 +0200

    Fetch data from a json file in nodeJS

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,18 +1,44 @@
 const express = require('express');
-const path = require('path');
-
+const { products } = require('./data')
 const app = express();
 
-app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+  res.send('<h1> Home Page</h1><a href="/api/products">products</a>');
 });
 
-app.all('*', (req, res) => {
-  res.status(404).send('Page not found');
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return {id, name, image};
+  });
+
+  res.json(newProducts);
 });
+
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
+
+   const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+    );
+
+    if(!singleProduct) {
+      return res.status(404).send('Product does not exist');
+    }
+
+  res.json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params)
+  res.send('hello world')
+})
+
 
 app.listen(5000, () => {
-  console.log('Server is listening to the port 5000');
+  console.log('Server is listening on port 5000');
 });
+
