6efe5516db3ed9914fe9b86021d183c316236790
sarazaToussaint <tmsaraza@gmail.com>
2023-06-13 23:40:38 +0200

    Make a change in a 13-router-people.js

--- 2-express-tutorial/final/13-router-people.js
+++ 2-express-tutorial/final/13-router-people.js
@@ -7,13 +7,7 @@ const {
   createPersonPostman,
   updatePerson,
   deletePerson,
-} = require('../controllers/people')
-
-// router.get('/', getPeople)
-// router.post('/', createPerson)
-// router.post('/postman', createPersonPostman)
-// router.put('/:id', updatePerson)
-// router.delete('/:id', deletePerson)
+} = require('../controllers/people');
 
 router.route('/').get(getPeople).post(createPerson)
 router.route('/postman').post(createPersonPostman)
