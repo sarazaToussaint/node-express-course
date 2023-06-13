4fc79b7aa2a5d3e77bd0ce0084f08da12562e159
sarazaToussaint <tmsaraza@gmail.com>
2023-06-13 23:29:19 +0200

    Make a change in a App.js

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -11,14 +11,3 @@ const logger = (req, res, next) => {
   console.log(method, url, time)
   next()
 }
-
-app.get('/', logger, (req, res) => {
-  res.send('Home')
-})
-app.get('/about', logger, (req, res) => {
-  res.send('About')
-})
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
