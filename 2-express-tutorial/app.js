17f93a4a9a8945c41b1d328c26ce5bd3215fd4f2
sarazaToussaint <tmsaraza@gmail.com>
2023-06-14 21:43:54 +0200

    Make a change

---
+++ 2-express-tutorial/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(req.url);
+  res.send('<h1>Home Page</h1><a href="/api/products">Product</a>')
+});
+
+app.get('/api/products', (req, res) => {
+   
+
+ res.status(200).send();
+});
+
+
+app.listen(5000, () => {
+   console.log('Server is listening on port 5000...'); 
+})
