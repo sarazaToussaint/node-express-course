879bf03a7d62401723613e8f3da839a0468b8162
sarazaToussaint <tmsaraza@gmail.com>
2023-06-14 22:24:34 +0200

    Make a change

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -10,10 +10,14 @@ app.get('/', (req, res) => {
 
 app.get('/api/products', (req, res) => {
   
+    // const newProducts = products.map((product) => {
+    //   const {id, name, image} = product;
+    //   return {id, name, image};
+    // });
     const newProducts = products.map((product) => {
-      const {id, name, image} = product;
-      return {id, name, image};
-    });
+        const { id, name, image } = product
+        return { id, name, image }
+      })
 
  res.status(200).json(newProducts);
 });
