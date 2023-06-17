850b9ae6749c90d380f398705c968335c0cb8b9a
sarazaToussaint <tmsaraza@gmail.com>
2023-06-17 23:28:43 +0200

    Create a server by using http

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,6 +1,12 @@
 const http = require('http');
 
  http.createServer((req, res) => {
+  const url = req.url;
+
+  if(url === '/'){
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.write('<h1>Home Page</h1>');
+  }
 
 }).listen(5000, () => {
   console.log('Server is listerning on port 5000...');
