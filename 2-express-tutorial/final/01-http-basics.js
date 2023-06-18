8d6646bd2f38ea5e3071cd870050ccbf6a4be51e
sarazaToussaint <tmsaraza@gmail.com>
2023-06-18 22:28:05 +0200

    Make change in 01-http-basics.js file

--- 2-express-tutorial/final/01-http-basics.js
+++ 2-express-tutorial/final/01-http-basics.js
@@ -1,9 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  
+  // console.log(req.method)
   const url = req.url
-  
+  // home page
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' })
     res.write('<h1>home page</h1>')
