26cd1e489e73ec4342dc4d269fc4bdebe73e67f0
sarazaToussaint <tmsaraza@gmail.com>
2023-06-18 22:24:40 +0200

    Make a change in a App.js

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -9,12 +9,6 @@ const server = http.createServer((req, res) => {
     res.write('<h1>home page</h1>')
     res.end()
   }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
 
 })
 
