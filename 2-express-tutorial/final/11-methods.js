e6ba4065511e3105458eb21f072013c62ea182b6
sarazaToussaint <tmsaraza@gmail.com>
2023-06-20 23:44:17 +0200

    Make a change in 04-express-app.js

--- 2-express-tutorial/final/11-methods.js
+++ 2-express-tutorial/final/11-methods.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
-let { people } = require('./data')
+const express = require('express');
+const app = express();
+let { people } = require('./data');
 
 // static assets
-app.use(express.static('./methods-public'))
+app.use(express.static('./methods-public'));
 // parse form data
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // parse json
-app.use(express.json())
+app.use(express.json());
 
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
