d487e16bcb8aa3f638eebdd236ff8a016e3b27c3
sarazaToussaint <tmsaraza@gmail.com>
2023-06-20 23:44:56 +0200

    Make a change in app.js

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -5,17 +5,17 @@ const app = express();
 
 app.use(express.static('./methods-public'));
 
-app.use(express.urlencoded({extended: false}))
+// app.use(express.urlencoded({extended: false}));
 
 app.get('/api/people', (req, res) => {
-  res.status(200).json({success:true, data: people})
+  res.status(200).json({success:true, data: people});
 });
 
 app.post('/login', (req, res) => {
   const {name} = req.body;
 
   if(name){
-   return res.status(200).send(`Welcome ${name}`)
+   return res.status(200).send(`Welcome ${name}`);
   }
 
   res.status(401).send('Please Provide Credentials');
