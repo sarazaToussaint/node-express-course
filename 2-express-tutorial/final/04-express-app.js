3d41adc285fc535e93cb507d8d6a941ca92020cc
sarazaToussaint <tmsaraza@gmail.com>
2023-06-20 23:44:25 +0200

    Make a change in 04-express-app.js

--- 2-express-tutorial/final/04-express-app.js
+++ 2-express-tutorial/final/04-express-app.js
@@ -9,7 +9,7 @@ app.use(express.static('./public'))
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
-
+ 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
 })
