eaa1880c6fc0bad0631bf9e4d3fd66520d2a7f3a
sarazaToussaint <tmsaraza@gmail.com>
2023-06-20 13:12:57 +0200

    Use of middleware in nodeJS

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,1 +1,26 @@
+const express = require('express');
+let { people } = require('./data');
 
+const app = express();
+
+app.use(express.static('./methods-public'));
+
+app.use(express.urlencoded({extended: false}))
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({success:true, data: people})
+});
+
+app.post('/login', (req, res) => {
+  const {name} = req.body;
+
+  if(name){
+   return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials');
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...');
+})
