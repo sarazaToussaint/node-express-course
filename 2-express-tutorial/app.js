f19889283f69a206f717f06fd07d7c52c0e4d67d
sarazaToussaint <tmsaraza@gmail.com>
2023-06-21 08:50:15 +0200

    Make a change in app.js

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -11,15 +11,15 @@ app.get('/api/people', (req, res) => {
   res.status(200).json({success:true, data: people});
 });
 
-// app.post('/login', (req, res) => {
-//   const {name} = req.body;
+app.post('/login', (req, res) => {
+  const {name} = req.body;
 
-//   if(name){
-//    return res.status(200).send(`Welcome ${name}`);
-//   }
+  if(name){
+   return res.status(200).send(`Welcome ${name}`);
+  }
 
-//   res.status(401).send('Please Provide Credentials');
-// })
+  res.status(401).send('Please Provide Credentials');
+})
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...');
