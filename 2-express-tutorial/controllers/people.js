0284bdf84d2f158874c34a54118f86e0b90b180c
sarazaToussaint <tmsaraza@gmail.com>
2023-06-22 12:10:05 +0200

    Destructure nodeJs codes

---
+++ 2-express-tutorial/controllers/people.js
@@ -0,0 +1,73 @@
+let { people } = require('../data');
+
+
+const getPeople = (req, res) => {
+  res.status(200).json({success:true, data: people});
+}
+
+const createPeople = (req, res) => {
+    const {name} = req.body;
+  
+    if(!name){
+      res
+      .status(400)
+      .json({ success: false, msg: 'Please provide a name value'});
+    }
+   res.status(201).json({ success: true, person: name });
+  }
+
+const createPostman = (req, res) => {
+    const {name} = req.body
+    
+    if(!name){
+      res
+      .status(400)
+      .json({ success: false, msg: 'Please provide a name value'});
+    }
+    res.status(201).json({ success: true, data: [...people, name] })
+}
+
+const updatePeople = (req, res) => {
+    const {id} = req.params
+    const {name} = req.body
+  
+    const person = people.find((person) => person.id === Number(id) );
+  
+    if(!person){
+      res
+      .status(400)
+      .json({ success: false, msg: `No person with id ${id}`});
+    }
+    const newPeople = people.map((person) => {
+      if(person.id === Number(id)){
+        person.name = name;
+      }
+      return person;
+    })
+  
+    res.status(200).json({ success: true, data: newPeople});
+}
+
+const deletePeople = (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id) );
+  
+    if(!person){
+      res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${req.params.id}`});
+    }
+  
+    const newPeople = people.filter(
+      (person) => person.id !== Number(req.params.id)
+      )
+  
+      return res.status(200).json({ success: true, data: newPeople });
+}
+
+module.exports = {
+  getPeople,
+  createPeople,
+  createPostman,
+  updatePeople,
+  deletePeople
+}
